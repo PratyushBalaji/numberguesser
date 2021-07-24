c941d3791a816bd99b79742a192336e058432d03
PratyushBalaji <59258572+PratyushBalaji@users.noreply.github.com>
2021-07-24 17:26:15 +0530

    Added colors and restart functionality
    Added colored text using colorama when someone wins or loses, also made it a function so it can be called again to restart game if user decides to play again

--- numberGuesser.py
+++ numberGuesser.py
@@ -1,44 +1,71 @@
 from random import randint
+import colorama
+from colorama import Fore,Back
+colorama.init()
 
-#intro questions for users to set parameters instead of opening source code to edit
-setMin = int(input("What should the smallest number on the scale be? "))
-setMax = int(input("What should the largest number on the scale be? "))
-setChances = int(input("How many chances would you like? "))
-lives = setChances
+def startGame():
 
-#variables for the game
-number = randint(setMin,setMax)
-chances = 0
+    #intro questions for users to set parameters instead of opening source code to edit
+    setMin = int(input("What should the smallest number on the scale be? "))
+    setMax = int(input("What should the largest number on the scale be? "))
+    setChances = int(input("How many chances would you like? "))
+    lives = setChances
 
-#pre-game instructions
-print("")
-print("")
-print("In this game, you have to guess the integer from ",setMin, "to ",setMax," picked at random.")
-print("You have a maximum of",setChances,"chances to do so.")
-print("")
-print("P.S. If you type anything other than integers, the game will exit...")
-print("")
-print("Happy gaming!")
-print("")
+    #variables for the game
+    number = randint(setMin,setMax)
+    chances = 0
 
+    #pre-game instructions
+    print(Fore.WHITE+"")
+    print("")
+    print("In this game, you have to guess the integer from ",setMin, "to ",setMax," picked at random.")
+    print("You have a maximum of",setChances,"chances to do so.")
+    print("")
+    print("P.S. If you type anything other than integers, the game will exit...")
+    print("")
+    print("Happy gaming!")
+    print("")
 
-#actual game code
-while chances <= lives-1:
-    guess = input("Pick a number between the numbers you chose - ")
-    guess = int(guess)
+    playing = True
 
-    if guess < number:
-        print("The number is higher than ",guess,"! Try again")
-        chances+=1
-    if guess > number:
-        print("The number is lower than ",guess,"! Try again")
-        chances+=1
-    if guess == number:
-        print("Yes! The number was ",number,"! You won! Press 'enter' to exit")
-        guess = input("")
-    if guess == '':
-        break
+    while playing:
+    #actual game code
+        while chances <= lives-1:
+            guess = input("Pick a number between the numbers you chose - ")
+            guess = int(guess)
 
-if not chances <= lives-1:
-    print("You have run out of chances!! The number was : ",number,"! Press 'enter' to exit")
-    input("Press 'enter' to exit the script ")
+            if guess < number:
+                print("The number is higher than ",guess,"! Try again")
+                chances+=1
+            if guess > number:
+                print("The number is lower than ",guess,"! Try again")
+                chances+=1
+            if guess == number:
+                print("")
+                print(Fore.GREEN+"Yes! The number was",number,Fore.GREEN+"! You Won!")
+                print("")
+                choice = input("Would you like to play again? y/n : ")
+                if choice.lower() == "n":
+                    print("Thanks for playing!")
+                    input()
+                    playing = False
+                elif choice.lower() == 'y':
+                    print("Lets play again!")
+                    startGame()
+            if guess == 'quit' or guess == '':
+                quit()
+
+        if not chances <= lives-1:
+            print("")
+            print(Fore.RED+"You have run out of chances!! The number was :",number)
+            print(Fore.WHITE+"")
+            choice = input("Would you like to play again? y/n : ")
+            if choice.lower() == "n":
+                print("Thanks for playing!")
+                input()
+                playing = False
+            elif choice.lower() == 'y':
+                print("Lets play again!")
+                startGame()
+
+startGame()
